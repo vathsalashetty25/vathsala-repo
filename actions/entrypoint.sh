b04ffea842d85c1570c24e6cd75e9388859e394e
vathsalashetty25 <70144077+vathsalashetty25@users.noreply.github.com>
2021-02-22 17:01:28 +0530

    Update entrypoint.sh

--- actions/entrypoint.sh
+++ actions/entrypoint.sh
@@ -1,3 +1,2 @@
-#!/bin/sh -l
-
-sh -c "echo Hello everyone, I'm $INPUT_MY_NAME and this is my first GitHub Action!"
+sh -c "echo Hello everyone, I'm $INPUT_MY_NAME and 
+this is my first GitHub Action!"
